d9af1d6a5a28ea09a2397fbe11305668d6e4c620
Sam Canning <samcanning@outlook.com>
2018-04-10 15:52:07 -0700

    moved host actions from admin controller to host controller

--- Controllers/HostController.cs
+++ Controllers/HostController.cs
@@ -12,6 +12,8 @@ using System.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Amazon.S3.Transfer;
+using System.IO;
 
 namespace PodcastSiteBuilder.Controllers
 {
@@ -25,6 +27,119 @@ namespace PodcastSiteBuilder.Controllers
             _context = context;
         }
 
+        [Route("admin/hosts/{id}")]
+        public IActionResult Host(int id)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == id);
+            if(thisHost == null) return RedirectToAction("EditHosts");
+            HostDisplay display = new HostDisplay()
+            {
+                id = id,
+                name = thisHost.name,
+                bio = thisHost.bio,
+                image = thisHost.image,
+                links = _context.Links.Where(l => l.host_id == thisHost.id).ToList()
+            };
+            return View(display);
+        }
+
+        [Route("admin/hosts/edit")]
+        public IActionResult EditHosts()
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            List<Host> hosts = _context.Hosts.ToList();
+            return View(hosts);
+        }
+
+        [Route("admin/hosts/add")]
+        public IActionResult AddHost()
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            return View();
+        }
+
+        [HttpPost]
+        [Route("admin/hosts/add/submit")]
+        public IActionResult CreateHost(Host model, IFormFile file)
+        {
+            if(model.name == null)
+            {
+                ModelState.AddModelError("name", "No name entered.");
+                return View("AddHost");
+            }
+            Host newHost = new Host(){name = model.name};
+            if(model.bio != null) newHost.bio = model.bio;
+            if(file != null)
+            {
+                TransferUtility transfer = new TransferUtility(Credentials.AccessKey, Credentials.SecretKey, Amazon.RegionEndpoint.USWest2);
+                using(var stream = new MemoryStream())
+                {
+                    string key = null;
+                    while(_context.Hosts.FirstOrDefault(h => h.image == key) != null) key = GenerateKey();
+                    file.CopyTo(stream);
+                    transfer.Upload(stream, "dhcimages", key);
+                    newHost.image = key;
+                }
+            }
+            _context.Add(newHost);
+            _context.SaveChanges();        
+            return RedirectToAction("Admin", "Admin");
+        }
+
+        [HttpPost]
+        public IActionResult AddImage(int id, IFormFile file)
+        {
+            TransferUtility transfer = new TransferUtility(Credentials.AccessKey, Credentials.SecretKey, Amazon.RegionEndpoint.USWest2);
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == id);
+            using(var stream = new MemoryStream())
+            {
+                string key = null;
+                while(_context.Hosts.FirstOrDefault(h => h.image == key) != null) key = GenerateKey();
+                file.CopyTo(stream);
+                transfer.Upload(stream, "dhcimages", key);
+                thisHost.image = key;
+            }
+            _context.Update(thisHost);
+            _context.SaveChanges();
+            return RedirectToAction("Host", new {id = id});
+        }
+
+        [Route("admin/hosts/delete")]
+        public IActionResult DeleteHost()
+        {
+            return View(_context.Hosts.ToList());
+        }
+
+        [Route("admin/hosts/delete/{id}")]
+        public IActionResult SubmitDeleteHost(int id)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host toDelete = _context.Hosts.SingleOrDefault(h => h.id == id);
+            if(toDelete == null) return RedirectToAction("DeleteHost");
+            List<Link> hostLinks = _context.Links.Where(l => l.host_id == id).ToList();
+            foreach(Link l in hostLinks)
+            {
+                _context.Remove(l);
+            }
+            _context.SaveChanges();
+            _context.Remove(toDelete);
+            _context.SaveChanges();
+            return RedirectToAction("Admin", "Admin");
+        }
+
+        [Route("admin/hosts/{hostid}/removelink/{linkid}")]
+        public IActionResult RemoveLink(int hostid, int linkid)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == hostid);
+            Link thisLink = _context.Links.SingleOrDefault(l => l.id == linkid);
+            if(thisLink == null || thisHost == null) return RedirectToAction("EditHosts");
+            _context.Remove(thisLink);
+            _context.SaveChanges();
+            return RedirectToAction("Host", new {id = hostid});
+        }
+
         [HttpPost]
         public IActionResult EditName(int id, string name)
         {
@@ -32,7 +147,7 @@ namespace PodcastSiteBuilder.Controllers
             thisHost.name = name;
             _context.Update(thisHost);
             _context.SaveChanges();
-            return RedirectToAction("Host", "Admin", new {id = id});
+            return RedirectToAction("Host", new {id = id});
         }
 
         [HttpPost]
@@ -42,22 +157,129 @@ namespace PodcastSiteBuilder.Controllers
             thisHost.bio = bio;
             _context.Update(thisHost);
             _context.SaveChanges();
-            return RedirectToAction("Host", "Admin", new {id = id});
+            return RedirectToAction("Host", new {id = id});
         }
 
-        [Route("admin/hosts/{hostId}/removelink/{linkId}")]
-        public IActionResult RemoveLink(int hostId, int linkId)
+        [Route("admin/hosts/{id}/addlink")]
+        public IActionResult AddLink(int id)
         {
-            if(HttpContext.Session.GetString("admin") != "true") return RedirectToAction("AdminLogin", "Admin");
-            Link toRemove = _context.Links.SingleOrDefault(l => l.id == linkId);
-            _context.Remove(toRemove);
-            _context.SaveChanges();
-            return RedirectToAction("Host", "Admin", new {id = hostId});
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == id);
+            if(thisHost == null) return RedirectToAction("EditHosts");
+            ViewBag.id = id;
+            ViewBag.name = thisHost.name;
+            return View();
         }
 
-        //need AddLink function!!
+        [HttpPost]
+        [Route("admin/hosts/{id}/addlink/submit")]
+        public IActionResult CreateLink(int id, string url, string site)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == id);
+            if(thisHost == null) return RedirectToAction("EditHosts");
+            ViewBag.name = thisHost.name;
+            ViewBag.id = id;
+            if(url == null) ModelState.AddModelError("url", "Must enter a URL.");
+            if(site == null) ModelState.AddModelError("site", "Must enter a title for your link.");
+            if(!ModelState.IsValid) return View("AddLink");
+            Link newLink = new Link(){
+                url = url,
+                site = site,
+                host_id = id
+            };
+            _context.Add(newLink);
+            _context.SaveChanges();
+            return RedirectToAction("Host", new {id = id});
+        }
+
+        [Route("admin/hosts/{hostid}/editlink/{linkid}")]
+        public IActionResult EditLink(int hostid, int linkid)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == hostid);
+            Link thisLink = _context.Links.SingleOrDefault(l => l.id == linkid);
+            if(thisLink == null || thisHost == null) return RedirectToAction("EditHosts");
+            ViewBag.name = thisHost.name;
+            ViewBag.id = hostid;
+            return View(thisLink);
+        }
+
+        [HttpPost]
+        [Route("admin/hosts/{hostid}/editlink/{linkid}/submit_url")]
+        public IActionResult EditLinkURL(Link model, int hostid, int linkid, string site)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == hostid);
+            Link thisLink = _context.Links.SingleOrDefault(l => l.id == linkid);
+            if(thisLink == null || thisHost == null) return RedirectToAction("EditHosts");
+            ViewBag.id = hostid;
+            ViewBag.name = thisHost.name;
+            if(model.url == null)
+            {
+                ModelState.AddModelError("url", "URL cannot be empty.");
+                thisLink.site = site;
+                return View("EditLink", thisLink);
+            }
+            thisLink.url = model.url;
+            _context.Update(thisLink);
+            _context.SaveChanges();
+            return RedirectToAction("Host", new {id = hostid});
+        }
+
+        [HttpPost]
+        [Route("admin/hosts/{hostid}/editlink/{linkid}/submit_title")]
+        public IActionResult EditLinkTitle(Link model, int hostid, int linkid, string url)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == hostid);
+            Link thisLink = _context.Links.SingleOrDefault(l => l.id == linkid);
+            if(thisLink == null || thisHost == null) return RedirectToAction("EditHosts");
+            ViewBag.id = hostid;
+            ViewBag.name = thisHost.name;
+            if(model.site == null)
+            {
+                ModelState.AddModelError("site", "Title cannot be empty.");
+                thisLink.url = url;
+                return View("EditLink", thisLink);
+            }
+            thisLink.site = model.site;
+            _context.Update(thisLink);
+            _context.SaveChanges();
+            return RedirectToAction("Host", new {id = hostid});
+        }
+
+        [Route("admin/hosts/{id}/removeimage")]
+        public IActionResult RemoveImage(int id)
+        {
+            if(NotLogged()) return RedirectToAction("AdminLogin", "Admin");
+            Host thisHost = _context.Hosts.SingleOrDefault(h => h.id == id);
+            if(thisHost == null) return RedirectToAction("Admin", "Admin");
+            thisHost.image = null;
+            _context.Update(thisHost);
+            _context.SaveChanges();
+            return RedirectToAction("Host", new {id = id});
+        }
 
         //need AddHost function!!
+
+        public bool NotLogged()
+        {
+            if(HttpContext.Session.GetString("admin") == "true") return false;
+            return true;
+        }
+
+        public string GenerateKey()
+        {
+            Random random = new Random();
+            string chars = "qwertyuiopasdfghjklzxcvbnm1234567890";
+            string key = "";
+            for(int i = 0; i < 16; i++)
+            {
+                key += chars[random.Next(chars.Length)];
+            }
+            return key;
+        }
 
     }
 }
